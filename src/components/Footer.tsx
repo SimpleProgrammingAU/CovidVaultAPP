f3401f0e4256dd67ca9e3a8a91e71740b439303a
Samuel Janda <sam.janda@simpleprogramming.com.au>
2020-08-22 17:22:18 +1000

    Updated footer accessibility and SEO

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default class Footer extends Component {
     return (
       <div className="Footer">
         <p>The CovidVault application is free for any Australian business.</p>
-        <p>This is an open-source project. The API source can be found <a href="https://github.com/SimpleProgrammingAU/CovidVaultAPI">here</a>. The application source can be found <a href="https://github.com/SimpleProgrammingAU/CovidVaultAPP">here</a>.</p>
+        <p>This is an open-source project - click thru to the <a href="https://github.com/SimpleProgrammingAU/CovidVaultAPI">API source</a> and the <a href="https://github.com/SimpleProgrammingAU/CovidVaultAPP">application source</a> at Github.</p>
         <p>Proudly hosted by <a href="https://www.simpleprogramming.com.au/">Simple Programming</a></p>
       </div>
     );
